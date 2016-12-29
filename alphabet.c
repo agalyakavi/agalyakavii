569a8fcb60ce8f9226dd66a967139981025dc807
agalyakavi <agalyakavi97@gmail.com>
2016-12-29 12:18:35 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if((ch>=65&&ch>90)||(ch>=97&&ch<=122))
+
+printf("alphabet");
+
+else
+  printf("not alphabet");
+  }
