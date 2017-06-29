2232323f164e08268c93e56010213c944ed03990
agalyakavi <agalyakavi97@gmail.com>
2017-06-29 11:41:10 +0530

    Add files via upload

---
+++ Gcd.java
@@ -0,0 +1,40 @@
+package set3;
+
+import java.util.Scanner;
+
+public class Gcd {
+	public static void main(String[]args){
+		Scanner read=new Scanner(System.in);
+		int a=read.nextInt();
+		int b=read.nextInt();
+		int t=a;
+		int t1=b;
+		int gcd;
+		while(true){
+			if(a>b){
+				if(a%b==0){
+					gcd=b;
+					break;
+				}
+				else
+				{
+					a=a%b;
+				}
+			}
+			else{
+				if(a<b){
+					if(b%a==0){
+						gcd=a;
+						break;
+					}
+					else
+					{
+						b=b%a;
+					}
+				
+			}
+		}
+	}
+System.out.println(gcd);
+}
+}
