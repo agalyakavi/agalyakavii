e0bb5f13a4abcee50e7c349429c60cff3030409d
agalyakavi <agalyakavi97@gmail.com>
2017-06-29 11:53:53 +0530

    Add files via upload

---
+++ Asciichar.java
@@ -0,0 +1,26 @@
+package set3;
+
+import java.util.Scanner;
+
+public class Asciichar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner read=new Scanner(System.in);
+		String a=read.nextLine();
+		for(int i=0;i<a.length();i++){
+			char b=a.charAt(i);
+			System.out.println((int)b);
+			
+		}
+
+	}
+
+	private static char[] valueOf(char b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
