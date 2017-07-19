fb7bd310c0ae93aa53a5116bf94b5b9a33019988
agalyakavi <agalyakavi97@gmail.com>
2017-07-19 10:24:29 +0530

    Add files via upload

---
+++ Set133.java
@@ -0,0 +1,23 @@
+package hunder;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Set133 {
+	public static void main(String[]args){
+		Scanner read=new Scanner(System.in);
+		int n=read.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=read.nextInt();
+		}
+		Arrays.sort(a);
+		for(int i=0;i<n;i++){
+			if(i==a[i]){
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+
+}
