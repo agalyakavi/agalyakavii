485d557ad43967d887b6d50a19e6719fc33cfa96
agalyakavi <agalyakavi97@gmail.com>
2017-07-20 11:09:08 +0530

    Add files via upload

---
+++ Arraymax.java
@@ -0,0 +1,23 @@
+package beginner;
+
+
+import java.util.Scanner;
+
+public class Arraymax {
+	public static void main(String[]args){
+		Scanner read=new Scanner(System.in);
+		int n=read.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=read.nextInt();
+		}
+		int sum=0;
+		int avg=0;
+		for(int i=0;i<n;i++){
+			sum+=a[i];
+		}
+		avg=sum/n;
+		System.out.println(avg);
+	}
+
+}
