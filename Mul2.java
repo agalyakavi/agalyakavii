1e8186da4c138e933bb13da8976c3c62572a72c0
agalyakavi <agalyakavi97@gmail.com>
2017-07-20 10:22:18 +0530

    Add files via upload

---
+++ Mul2.java
@@ -0,0 +1,14 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class Mul2 {
+	public static void main(String []args){
+		Scanner read=new Scanner(System.in);
+		int n=read.nextInt();
+		if(n%2==0)
+			System.out.println(+ n +"is the power of 2");
+		
+	}
+
+}
