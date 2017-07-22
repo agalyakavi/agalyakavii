55f6a52904a63443a1386f4aa42ebc6bc2a7bb8e
agalyakavi <agalyakavi97@gmail.com>
2017-07-22 15:22:46 +0530

    Add files via upload

---
+++ Big3.java
@@ -0,0 +1,11 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class Big3 {
+public static void main(String[]args){
+	Scanner read=new Scanner(System.in);
+	String a=read.nextLine();
+	System.out.println(a+","+a.substring(0,1));
+}
+}
