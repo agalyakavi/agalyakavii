b46bf88ab89588096a49109875e5a937c7cea423
agalyakavi <agalyakavi97@gmail.com>
2017-07-26 15:17:56 +0530

    Add files via upload

---
+++ Strlen.java
@@ -0,0 +1,13 @@
+package player;
+
+import java.util.Scanner;
+
+public class Strlen {
+	public static void main(String[]args){
+		Scanner read=new Scanner(System.in);
+		String str=read.nextLine();
+		int n=str.length();
+		System.out.println("The String Length is"+n);
+	}
+
+}
