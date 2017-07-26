bb49ae4cbc91babe41539a283658d4457828a74d
agalyakavi <agalyakavi97@gmail.com>
2017-07-26 17:03:53 +0530

    Add files via upload

---
+++ Sumofadd.java
@@ -0,0 +1,16 @@
+package player;
+
+public class Sumofadd {
+	public static void main(String[]args){
+		int sum=0;
+		for(int i=0;i<20;i++){
+			if(i%2==1){
+				sum+=i;
+				
+			}
+			
+		}
+		System.out.println(sum);
+	}
+
+}
